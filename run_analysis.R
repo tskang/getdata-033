aa046c624c0acb1e2d0aab59d25f0dd32d77fee3
tskang <tskang3@gmail.com>
2015-10-21 23:55:39 -0400

    q2

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,45 @@
+# This code does the following. 
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average 
+#    of each variable for each activity and each subject.
+
+# 1. Merge the training and the test sets to create one data set.
 # read the training set
-d1 <- read.csv("X_train.txt", header = FALSE, sep = " ")
-str(d1) # 'data.frame':	11228 obs. of  662 variables
+# d1 <- read.csv("X_train.txt", header = FALSE, sep = " ")
+# str(d1) # 'data.frame':	11228 obs. of  662 variables
 
 # read the test set
-d2 <- read.csv("X_test.txt", header = FALSE, sep = " ")
-str(d2) # 'data.frame':	4312 obs. of  667 variables
+# d2 <- read.csv("X_test.txt", header = FALSE, sep = " ")
+# str(d2) # 'data.frame':	4312 obs. of  667 variables
 
-merged <- merge(d1, d2, all = TRUE)
-str(merged)
-write.table(merged, file = "X_merged.txt", row.name=FALSE)
+# merged <- merge(d1, d2, all = TRUE)
+merged <- read.csv("X_merged.txt", header = FALSE, sep = " ")
+# str(merged)
+# write.table(merged, file = "X_merged.txt", row.name=FALSE)
+
+# 2. Extract only the measurements on the mean and standard deviation for each measurement. 
+# read the features
+f <- read.csv("features.txt", sep = " ", header = FALSE)
+str(f) # 'data.frame':	561 obs. of  2 variables:
+# grep mean and standard deviation: mean 33, std 33
+m <- subset(f, grepl("mean\\(\\)", V2))
+s <- subset(f, grepl("std\\(\\)", V2))
+str(m)
+str(s)
+ms <- rbind(m, s) # 66 obs.
+str(ms)
+mms <- merged[ms$V1]
+str(mms) # 'data.frame':	15541 obs. of  66 variables:
+colnames(mms) <- ms$V2
+write.table(mms, file = "X_merged_mean_std.txt", row.name=FALSE)
+
+# 3. Uses descriptive activity names to name the activities in the data set
+
+# 4. Appropriately labels the data set with descriptive variable names. 
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average 
+#    of each variable for each activity and each subject.
 
