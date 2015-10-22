530cf0df59c1c2e857c414406a93edeab9ad8a41
tskang <tskang3@gmail.com>
2015-10-22 01:43:22 -0400

    merge

--- run_analysis.R
+++ run_analysis.R
@@ -8,17 +8,22 @@
 
 # 1. Merge the training and the test sets to create one data set.
 # read the training set
-# d1 <- read.csv("X_train.txt", header = FALSE, sep = " ")
-# str(d1) # 'data.frame':	11228 obs. of  662 variables
+d11 <- read.table("train/subject_train.txt")
+d12 <- read.table("train/X_train.txt")
+d13 <- read.table("train/y_train.txt")
+d1 <- cbind(d11, d12, d13)
 
 # read the test set
-# d2 <- read.csv("X_test.txt", header = FALSE, sep = " ")
-# str(d2) # 'data.frame':	4312 obs. of  667 variables
+d21 <- read.table("test/subject_test.txt")
+d22 <- read.table("test/X_test.txt")
+d23 <- read.table("test/y_test.txt")
+d2 <- cbind(d21, d22, d23)
 
 # merged <- merge(d1, d2, all = TRUE)
-merged <- read.csv("X_merged.txt", header = FALSE, sep = " ")
+merged <- rbind(d1, d2)
+# merged <- read.csv("X_merged.txt", header = FALSE, sep = " ")
 # str(merged)
-# write.table(merged, file = "X_merged.txt", row.name=FALSE)
+write.table(merged, file = "merged.txt", row.name=FALSE)
 
 # 2. Extract only the measurements on the mean and standard deviation for each measurement. 
 # read the features
@@ -27,19 +32,20 @@ str(f) # 'data.frame':	561 obs. of  2 variables:
 # grep mean and standard deviation: mean 33, std 33
 m <- subset(f, grepl("mean\\(\\)", V2))
 s <- subset(f, grepl("std\\(\\)", V2))
-str(m)
-str(s)
+# str(m)
+# str(s)
 ms <- rbind(m, s) # 66 obs.
-str(ms)
+# str(ms)
 mms <- merged[ms$V1]
-str(mms) # 'data.frame':	15541 obs. of  66 variables:
-colnames(mms) <- ms$V2
-write.table(mms, file = "X_merged_mean_std.txt", row.name=FALSE)
+# str(mms) # 'data.frame':	15541 obs. of  66 variables:
 
 # 3. Uses descriptive activity names to name the activities in the data set
+colnames(mms) <- ms$V2
+write.table(mms, file = "merged_mean_std.txt", row.name=FALSE)
 
 # 4. Appropriately labels the data set with descriptive variable names. 
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average 
 #    of each variable for each activity and each subject.
+
 
